56218ecc2188c878976b8df7c30065a2276880b1
Terrence Curran <tea@approachingpi.com>
2013-01-16 03:57:55 -0500

    file parse outputting content.

--- parser/src/main/java/com/wirelust/appleworks/Parser.java
+++ parser/src/main/java/com/wirelust/appleworks/Parser.java
@@ -7,6 +7,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -14,6 +16,8 @@ import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.CountingInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,6 +34,9 @@ import org.slf4j.LoggerFactory;
 public class Parser {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Parser.class);
+
+	static final byte[] KEYWORD_DSET	= {0x44, 0x53, 0x45, 0x54};
+
 
 	private String opt_file;
 
@@ -53,11 +60,10 @@ public class Parser {
 
 			File file = new File(opt_file);
 
-			DataInputStream dataInputStream = new DataInputStream(
-					new BufferedInputStream(
-							new FileInputStream(file)
-					)
-			);
+			FileInputStream fileInputStream = new FileInputStream(file);
+			BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+			CountingInputStream countingInputStream = new CountingInputStream(bufferedInputStream);
+			DataInputStream dataInputStream = new DataInputStream(countingInputStream);
 
 			// version
 			// 06 07 E1 00
@@ -144,12 +150,95 @@ public class Parser {
 			short page_inner_width = dataInputStream.readShort();
 			println("page inner width=%d", page_inner_width);
 
+			// this isn't really the end of the header. I just don't know where it ends yet.
+			int header_end_position = countingInputStream.getCount();
+
+
+			// TODO: figure out a more efficient way to do this.
+			// for now, we are just reading the whole file into an array
+
+			FileInputStream fileInputStream2 = new FileInputStream(file);
+			BufferedInputStream bufferedInputStream2 = new BufferedInputStream(fileInputStream2);
+
+
+			byte allBytes[] = IOUtils.toByteArray(bufferedInputStream2);
+
+			int content_start_position = 0;
+			int content_end_position = 0;
+			boolean content_start_found = false;
+
+			int dsetBlocks = 5; // all DSETs have beeen observed to have 5 blocks
+			int dsetIndex = 0;
+			byte[] keyword = new byte[4];
+			for (int i=0; i<allBytes.length; i++) {
+				keyword = Arrays.copyOfRange(allBytes, i, i + keyword.length);
+
+				int blockPosition = i + keyword.length;
+				if (Arrays.equals(keyword, KEYWORD_DSET)) {
+					println("DSET found at position:%d", i);
+					dsetIndex++;
+
+					for (int blockIndex = 0; blockIndex < dsetBlocks; blockIndex++) {
+						int blockLen = getArrayInt(allBytes, blockPosition);
+						println("\tdset:%d, block:%d, length:%d", dsetIndex, blockIndex, blockLen);
+
+						blockPosition = blockPosition + 4 + blockLen;
+					}
+				}
+
+				if (dsetIndex == 1 && !content_start_found) {
+					content_start_found = true;
+					content_start_position = blockPosition;
+				}
+			}
+
+			if (content_start_found) {
+				println("content starts at position=%d", content_start_position);
+
+				boolean content_end_found = false;
+				int blockPosition = content_start_position;
+				int contentBlockIndex = 0;
+				while (!content_end_found) {
+					int blockLen = getArrayInt(allBytes, blockPosition);
+
+					String block = new String(Arrays.copyOfRange(allBytes, blockPosition + 4, blockPosition + 4 + blockLen), "MacRoman");
+
+					println("\tcontent block:%d, length:%d", contentBlockIndex, blockLen);
+
+					String block2 = block.replaceAll("\r", "\n");
+
+					System.out.println(block2);
+
+					// TODO: convert strings into UTF
+
+					blockPosition = blockPosition + 4 + blockLen;
+					contentBlockIndex++;
+
+					if (block.charAt(block.length()-1) == 0x00) {
+						content_end_found = true;
+						content_end_position = blockPosition;
+					}
+
+				}
+
+				println("content ends at position=%d", content_end_position);
+			}
 
 		} catch (IOException e) {
 			LOGGER.error("ioexception", e);
 		}
 
 
+	}
+
+	public int getArrayInt(byte[] array, int position) {
+		ByteBuffer wrapped = ByteBuffer.wrap(Arrays.copyOfRange(array, position, position + 4));
+		return wrapped.getInt();
+	}
+
+	public short getArrayShort(byte[] array, int position) {
+		ByteBuffer wrapped = ByteBuffer.wrap(Arrays.copyOfRange(array, position, position + 2));
+		return wrapped.getShort();
 	}
 
 	public void println(String line, Object... args) {
